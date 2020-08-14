1639fc0b7cd50ef883bcf6b7d8811730eeea2c3f
Don <hello@donbos.co>
2020-08-14 19:35:36 +0800

    initialize project with create react app, add basic dependencies, and begin project structure

---
+++ src/components/SearchBar.js
@@ -0,0 +1,16 @@
+import React from "react"
+import styled from "styled-components"
+
+
+const Paragraph = styled.p`
+    background: yellow;
+    font-weight: bold;
+`
+
+const SearchBar = () => {
+    return (
+        <Paragraph>Hello world</Paragraph>
+    )
+}
+
+export default SearchBar;
