f3b04ff709db9118791eaab605e05cf13ae35754
Don <hello@donbos.co>
2020-08-15 15:56:09 +0800

    search for character query setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import styled from "styled-components"
 
 import SearchBar from './components/SearchBar';
 
+const Wrapper = styled.div`
+  padding: 15px;
+  max-width: 800px;
+  width: 100%;
+  margin: auto;
+`
+
 function App() {
   return (
-    <SearchBar />
+    <Wrapper>
+      <SearchBar/>
+    </Wrapper>
   );
 }
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,46 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import styled from "styled-components"
 
+import { fetchPeople, searchPerson } from "../actions/peopleList";
 
 const Paragraph = styled.p`
     background: yellow;
     font-weight: bold;
 `
 
-const SearchBar = () => {
+const SearchInput = styled.input`
+    padding: 5px;
+    -webkit-appearance: none;
+    appearance: none;
+    border-radius: 3px;
+    border: 1px solid #ccc;
+`
+
+const SearchBar = () => {  
+
+    const [ query, setQuery ] = useState("");
+
+    const inputQuery = e => {
+        setQuery(e.target.value);
+    }
+
+    useEffect(() => {
+        if (query) {
+            console.log("Searching...");
+            searchPerson(query);
+        }
+        
+    }, [query]);
+    
     return (
-        <Paragraph>Hello world</Paragraph>
+        <>
+            <Paragraph>Hello world</Paragraph>
+            <SearchInput
+                onChange={inputQuery}
+                type="text"
+                placeholder="Search for a character"
+            />
+        </>
     )
 }
 
