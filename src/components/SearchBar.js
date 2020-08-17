3a2f55e6b5f8b8a8347f9daec738c646b172dde5
Don <hello@donbos.co>
2020-08-17 23:04:16 +0800

    setting up typeahead to search for characters and render them

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,46 +1,66 @@
-import React, { useState, useEffect } from "react"
-import styled from "styled-components"
+import React, { Fragment, useState, useEffect } from "react";
+import axios from 'axios';
+import { AsyncTypeahead } from 'react-bootstrap-typeahead';
+import styled from "styled-components";
 
-import { fetchPeople, searchPerson } from "../actions/peopleList";
-
-const Paragraph = styled.p`
-    background: yellow;
-    font-weight: bold;
-`
-
-const SearchInput = styled.input`
-    padding: 5px;
-    -webkit-appearance: none;
-    appearance: none;
-    border-radius: 3px;
-    border: 1px solid #ccc;
+const SearchInput = styled(AsyncTypeahead)`
+    input {
+        padding: 5px;
+        width: 100%;
+        -webkit-appearance: none;
+        appearance: none;
+        border-radius: 3px;
+        border: 1px solid #ccc;
+        font-size: larger;
+    }
 `
 
 const SearchBar = () => {  
+    const [isLoading, setIsLoading] = useState(false);
+    const [options, setOptions] = useState([]);
 
-    const [ query, setQuery ] = useState("");
+    const handleSearch = (query) => {
+        axios.get(`https://swapi.dev/api/people/?search=${query}`)
+            .then((response) => response.data)
+            .then(({ results }) => {
+                console.log("================")
+                const options = results.map((i) => ({
+                    name: i.name,
+                    url: i.url,
+                    homeworld_url: i.homeworld,
+                    species_url: i.species[0],
+                }));
 
-    const inputQuery = e => {
-        setQuery(e.target.value);
-    }
+                setOptions(options);
+                setIsLoading(false);
 
-    useEffect(() => {
-        if (query) {
-            console.log("Searching...");
-            searchPerson(query);
-        }
-        
-    }, [query]);
+                return options;
+            })
+            .then((options) => {
+                console.log(typeof options);
+            });
+    };
     
     return (
-        <>
-            <Paragraph>Hello world</Paragraph>
+        <Fragment>
             <SearchInput
-                onChange={inputQuery}
-                type="text"
-                placeholder="Search for a character"
-            />
-        </>
+                id="characterSearch"
+                isLoading={isLoading}
+                labelKey={(option) => `${option.name}`}
+                minLength={2}
+                onSearch={handleSearch}
+                options={options}
+                placeholder="Search for a character..."
+                renderMenuItemChildren={(option) => (
+                    <Fragment>
+                        <p>{option.name} ({option.species_url})</p>
+                        <p>From {option.homeworld_url} (population:)</p>
+                        <p>Link: {option.url}</p>
+                    </Fragment>
+                )}
+                />
+            
+        </Fragment>
     )
 }
 
