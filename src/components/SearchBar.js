10a3468a107e62f0546fb29bac55af6ff4684bea
Don <hello@donbos.co>
2020-08-18 18:13:22 +0800

    import api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect, useRef } from "react";
 import { useHistory } from "react-router-dom";
 import axios from 'axios';
+import { getSpeciesInfo, getHomeworldInfo } from '../lib/api';
 import { AsyncTypeahead } from 'react-bootstrap-typeahead';
 import styled from "styled-components";
 
@@ -51,42 +52,6 @@ const SearchBar = () => {
     useEffect(()=>{
         ref.current.focus();
     }, [])
-
-    const getSpeciesInfo = url => {
-        return axios.get(`${url}`)
-            .then((response) => response.data)
-            .then((data) => {
-                // Species seems to be undefined for humans only
-                let species = data.name;
-                if (typeof species === 'undefined') {
-                    species = "Human"
-                }
-
-                return {
-                    species: species,
-                }
-            })
-            .catch(error => { console.error(error); return Promise.reject(error); });
-    }
-
-    const getHomeworldInfo = url => {
-        return axios.get(`${url}`)
-            .then((response) => response.data)
-            .then((data) => {
-
-                // Prettify number by adding commas after every 3 digits
-                let homeworld_population = data.population;
-                if (homeworld_population !== 'unknown') {
-                    homeworld_population = parseInt(homeworld_population).toLocaleString();
-                }
-
-                return {
-                    homeworld: data.name,
-                    homeworld_population: homeworld_population
-                }
-            })
-            .catch(error => { console.errors(error); return Promise.reject(error); });
-    }
 
     const handleSearch = query => {
         setIsLoading(true);
