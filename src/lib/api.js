d2ad52ca94e32057c077cb1ab678d0e7e6be7290
Don <hello@donbos.co>
2020-08-19 02:50:49 +0800

    clip opening crawl to 150 chars

--- src/lib/api.js
+++ src/lib/api.js
@@ -4,7 +4,7 @@ export const getSpeciesInfo = url => {
     return axios.get(`${url}`)
         .then((response) => response.data)
         .then((data) => {
-            // Species seems to be undefined for humans only
+            // If the species field is undefined, it seems to mean they are humans
             let species = data.name;
             if (typeof species === 'undefined') {
                 species = "Human"
@@ -14,14 +14,17 @@ export const getSpeciesInfo = url => {
                 species: species,
             }
         })
-        .catch(error => { console.error(error); return Promise.reject(error); });
+        .catch(error => {
+            console.log("there's an error with the getSpeciesInfo promise!");
+            console.error(error);
+            return Promise.reject(error);
+        });
 }
 
 export const getHomeworldInfo = url => {
     return axios.get(`${url}`)
         .then((response) => response.data)
         .then((data) => {
-
             // Prettify number by adding commas after every 3 digits
             let homeworld_population = data.population;
             if (homeworld_population !== 'unknown') {
@@ -31,6 +34,28 @@ export const getHomeworldInfo = url => {
             return {
                 homeworld: data.name,
                 homeworld_population: homeworld_population
+            }
+        })
+        .catch(error => {
+            console.log("there's an error with the getHomeworldInfo promise!");
+            console.errors(error);
+            return Promise.reject(error);
+        });
+}
+
+export const getFilmInfo = url => {
+    return axios.get(`${url}`)
+        .then((response) => response.data)
+        .then((data) => {
+
+            // Clip opening crawl to first 150 characters
+            const clipped_opening_crawl = data.opening_crawl.substring(0, 150);
+
+            return {
+                title: data.title,
+                release_date: data.release_date,
+                opening_crawl: data.opening_crawl,
+                clipped_opening_crawl: clipped_opening_crawl
             }
         })
         .catch(error => { console.errors(error); return Promise.reject(error); });
