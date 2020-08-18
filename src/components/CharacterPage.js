8d35f77e81aff0c4ae9030fad8304875d3b4ec08
Don <hello@donbos.co>
2020-08-19 01:13:44 +0800

    resolve promises for GET multiple films

--- src/components/CharacterPage.js
+++ src/components/CharacterPage.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useState, useEffect } from "react";
 import axios from 'axios';
-import { getSpeciesInfo, getHomeworldInfo } from '../lib/api';
+import { getSpeciesInfo, getHomeworldInfo, getFilmInfo } from '../lib/api';
 import styled from "styled-components";
 
 const Title = styled.h1`
@@ -14,60 +14,27 @@ const Footnote = styled.small`
 const CharacterPage = ({match}) => {
 
     const [ characterDetails, setCharacterDetails ] = useState([])
-
-    const getMultipleFilmInfo = async obj => {
-        const promises = obj.map((i) => {
-            axios.get(i)
-                .then((response) => response.data)
-                .then(data => {
-                    // console.log(data);
-
-                    return {
-                        title: data.title,
-                        opening_crawl: data.opening_crawl,
-                        release_date: data.release_date
-                    }
-                })
-        });
-
-        console.log(promises);
-    };
-
-    const getFilmInfo = url => {
-        return axios.get(`${url}`)
-        .then((response) => response.data)
-        .then((data) => {
-
-            // Prettify number by adding commas after every 3 digits
-            console.log(data);
-            return {
-                film_title: data.title,
-                film_release_date: data.release_date,
-                film_opening_crawl: data.opening_crawl
-            }
-        })
-        .catch(error => { console.errors(error); return Promise.reject(error); });
-    }
     
     const getCharacterInfo = (id) => {
         axios.get(`https://swapi.dev/api/people/${id}`)
             .then((response) => response.data)
             .then(async data => {
                 const name = data.name;
-                // const filmRequests = 
-                
+                const filmRequests = data.films.map(film => getFilmInfo(film));
+
+                const filmResolvedPromise = await Promise.all(filmRequests);
+
                 const promises = Promise.all([
                     getHomeworldInfo(data.homeworld),
                     getSpeciesInfo(data.species),
-                    getFilmInfo(data.films[0])
                 ])
                 .then(data => {
-                    console.log(data);
                     return {
                         name: name,
                         homeworld: data[0].homeworld,
                         homeworld_population: data[0].homeworld_population,
-                        species: data[1].species
+                        species: data[1].species,
+                        films: filmResolvedPromise
                     }
                 })
 
@@ -76,6 +43,7 @@ const CharacterPage = ({match}) => {
 
                 setCharacterDetails(results);
             })
+            
     }
 
     useEffect(() => {
