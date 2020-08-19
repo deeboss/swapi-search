3f9b529d3e17a6bab6974644fd5056004ad294d0
Don <hello@donbos.co>
2020-08-19 21:48:12 +0800

    simple template for Character Card

---
+++ src/components/CharacterCard.js
@@ -0,0 +1,14 @@
+import React, { Fragment, useState, useEffect } from "react";
+import styled from "styled-components";
+
+
+const CharacterCard = (results) => {
+    const [ characterList, setCharacterList ] = useState([]);
+    
+    return (
+        <Fragment>
+        </Fragment>
+    )
+}
+
+export default CharacterCard;
