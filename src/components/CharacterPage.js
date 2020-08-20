c3edbe8a1d109da3d1a67524deeef5285675aed7
Don <hello@donbos.co>
2020-08-20 19:43:41 +0800

    close button added

--- src/components/CharacterPage.js
+++ src/components/CharacterPage.js
@@ -1,12 +1,49 @@
 import React, { Fragment, useState, useEffect } from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+
 import { getCharacterInfo } from '../lib/api';
 import { retrieveBasicCharacterInfo, retrieveFilmDetails } from '../lib/util';
 
-import styled from "styled-components";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+
 
 import FilmList from './FilmList';
 
+const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid white;
+    padding-bottom: 0.75em;
+    margin-bottom: 2.5em;
+`
+
+const CloseButton = styled.span`
+    cursor: pointer;
+    background: rgba(255,255,255,0);
+    padding: 15px;
+    border-radius: 40px;
+
+    svg {
+        pointer-events: none;
+    }
+
+    &:hover {
+        background: rgba(255,255,255,0.12);
+    }
+`
+
+const Container = styled.div`
+`
+
 const Title = styled.h1`
+
+`
+
+
+const Subtitle = styled.p`
 
 `
 
@@ -15,8 +52,12 @@ const SpeciesText = styled.span`
 `
 
 const CharacterPage = ({match}) => {
-
     const [ characterDetails, setCharacterDetails ] = useState({})
+    const history = useHistory();
+
+    const handleClose = () => {
+        history.push(`/`);
+    }
 
     const handleCharacterRequest = async (id) => {
         try {
@@ -40,23 +81,31 @@ const CharacterPage = ({match}) => {
 
     return (
         <Fragment>
-            <Title>
-                {characterDetails.name}
+            <Header>
+                <Container>
+                    <Title>
+                        {characterDetails.name}
 
-                { characterDetails.species &&
-                    <SpeciesText>({characterDetails.species})</SpeciesText>
-                }
-            </Title>
-            { characterDetails.homeworld_name &&
-                <p>From {characterDetails.homeworld_name} (population: {characterDetails.homeworld_population})</p>
-            }
+                        { characterDetails.species &&
+                            <SpeciesText>({characterDetails.species})</SpeciesText>
+                        }
+                    </Title>
+                    { characterDetails.homeworld_name &&
+                        <Subtitle>From {characterDetails.homeworld_name} (population: {characterDetails.homeworld_population})</Subtitle>
+                    }
+                </Container>
+                <Container>
+                    <CloseButton onClick={handleClose}>
+                        <FontAwesomeIcon icon={faTimes} size="lg" />
+                    </CloseButton>
+                </Container>
+            </Header>
             <div>
                 { characterDetails.films_arr &&
                     <Fragment>
-                        <hr />
                         <h4>Films appeared in:</h4>
                         <div>
-                            { characterDetails.films ? <FilmList films={characterDetails.films}/> : <p>Loading...</p> }
+                            { characterDetails.films && <FilmList films={characterDetails.films}/> }
                         </div>
                     </Fragment>
                 }
