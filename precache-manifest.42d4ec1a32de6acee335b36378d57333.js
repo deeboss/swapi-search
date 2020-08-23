4d8c99dc8fcfe9ccb11d3b8dedc35ad9ec00daf0
Don <hello@donbos.co>
2020-08-24 00:41:53 +0800

    Updates

---
+++ precache-manifest.42d4ec1a32de6acee335b36378d57333.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e836c14e58c74bcc5b8fd0d3467cb2",
+    "url": "/swapi-search/index.html"
+  },
+  {
+    "revision": "f99e44087315ac84e7c0",
+    "url": "/swapi-search/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "4d1f1a9c021537834017",
+    "url": "/swapi-search/static/css/main.dca04ac4.chunk.css"
+  },
+  {
+    "revision": "f99e44087315ac84e7c0",
+    "url": "/swapi-search/static/js/2.485ee8aa.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/swapi-search/static/js/2.485ee8aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d1f1a9c021537834017",
+    "url": "/swapi-search/static/js/main.554a6447.chunk.js"
+  },
+  {
+    "revision": "4d209328114f118a7fc8",
+    "url": "/swapi-search/static/js/runtime-main.19110520.js"
+  }
+]);
