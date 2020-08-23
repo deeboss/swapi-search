af596bfb05cd16639a16a383c2ae688dd400381c
Don <hello@donbos.co>
2020-08-24 02:48:12 +0800

    add missing closing slash to ensure proper https GET request

--- src/lib/api.js
+++ src/lib/api.js
@@ -28,9 +28,12 @@ export const getPageResults = async (url) => {
 };
 
 export const getCharacterInfo = async (id, cancelToken) => {
-  const httpsUrl = `https://swapi.dev/api/people/${id}/`;
-  console.log(httpsUrl);
-  const response = await axios.get(httpsUrl, {
+  // const httpsUrl = `https://swapi.dev/api/people/${id}/`;
+  // console.log(httpsUrl);
+  // const response = await axios.get(httpsUrl, {
+  //   cancelToken: cancelToken,
+  // });
+  const response = await axios.get(`https://swapi.dev/api/people/${id}/`, {
     cancelToken: cancelToken,
   });
   const data = response.data;
